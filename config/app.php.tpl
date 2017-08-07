e4cd9316f7c9eb266b2de13f10f480603d73b45e
Jim Bennett <jim@advancebass.com>
2017-08-07 13:22:29 -0700

    init

--- config/app.php.tpl
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config = array(
-  'twig_cache' => TEMPLATEPATH . '/cache',
-);
-
-?>
--- functions.php
+++ functions.php
@@ -1,17 +1,5 @@
 <?php
 
-  require_once('config/app.php');
-  require_once('lib/init.php');
-  require_once('lib/twig/init.php');
-  require_once('lib/utils.php');
-  require_once('lib/view_loader.php');
-  require_once('lib/view.php');
-  require_once('lib/roots/cleanup.php');
-  require_once('lib/roots/scripts.php');
-  require_once('models/blog.php');
-  require_once('models/post.php');
-  require_once('models/author.php');
-  require_once('models/comment.php');
-  require_once('models/thumbnail.php');
+  // require_once('config/app.php');
 
 ?>
